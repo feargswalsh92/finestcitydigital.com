0a8fdd255134d08e5f5b9d9607862b71d971a5f0
feargswalsh92 <feargswalsh@gmail.com>
2024-10-17 10:50:28 -0700

    insert all components into site

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -115,3 +115,13 @@ document.addEventListener('scroll', () => {
         }
     }, 50);
 });
+
+
+const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
+        for (const item of faqItems) {
+            const onClick = () => {
+            item.classList.toggle('active')
+        }
+        item.addEventListener('click', onClick)
+        }
+              
