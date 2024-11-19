88f7fecaa8096a61882b9ea96264c23844cb1dda
feargswalsh92 <feargswalsh@gmail.com>
2024-11-18 17:35:32 -0800

    update copy

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,19 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Finest City Digital",
+    "email": "f@finestcitydigital.com",
+    "phoneForTel": "619-432-5847",
+    "phoneFormatted": "619-432-5847",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "lineOne": "4429 Marlborough Avenue",
+        "city": "San Diego",
+        "state": "CA",
+        "zip": "92116",
         "country": "US",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "mapLink": "https://maps.app.goo.gl/vbprDS2m55f2wdqq8"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        // "facebook": "https://www.facebook.com/",
+        // "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
     "domain": "https://www.example.com"
